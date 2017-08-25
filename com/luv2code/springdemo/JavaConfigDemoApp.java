bf55868c2c8f3b400e492cdd04f4208af4076971
Jakub Kaczynski <jakubkaczynski93@gmail.com>
2017-08-25 20:23:49 +0200

    Spring configuration with Java Code

---
+++ com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,35 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		
+		//read spring config java class
+		AnnotationConfigApplicationContext context=
+				new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		//get bean from spring container
+		//Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		Coach theCoach1 = context.getBean("runningCoach", Coach.class);
+		
+		//call a method on the bean
+		//System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach1.getDailyWorkout());
+
+		// call method to get the daily fortune
+		//System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach1.getDailyFortune());
+		
+		
+		//close the context
+		context.close();
+		
+		
+		
+
+	}
+
+}
