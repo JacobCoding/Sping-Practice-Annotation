043f5914b3ea168244f4343d1b91efb1ba5d3b45
Jakub Kaczynski <jakubkaczynski93@gmail.com>
2017-08-26 14:13:01 +0200

    Injecting Values from Properties File

--- com/luv2code/springdemo/SwimCoach.java
+++ com/luv2code/springdemo/SwimCoach.java
@@ -1,11 +1,18 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
 	public SwimCoach(FortuneService fortuneService) {
-		super();
 		this.fortuneService = fortuneService;
 	}
 
@@ -18,5 +25,18 @@ public class SwimCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+	
+	
+	
+	
+	
 
 }
--- com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -13,16 +13,19 @@ public class SwimJavaConfigDemoApp {
 		
 		//get bean from spring container
 		//Coach theCoach = context.getBean("tennisCoach", Coach.class);
-		Coach theCoach1 = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach2 = context.getBean("swimCoach", SwimCoach.class);
 		
 		//call a method on the bean
 		//System.out.println(theCoach.getDailyWorkout());
-		System.out.println(theCoach1.getDailyWorkout());
+		System.out.println(theCoach2.getDailyWorkout());
 
 		// call method to get the daily fortune
 		//System.out.println(theCoach.getDailyFortune());
-		System.out.println(theCoach1.getDailyFortune());
+		System.out.println(theCoach2.getDailyFortune());
 		
+		//call new swim coach methods ... has the props values injected
+		System.out.println("email: " + theCoach2.getEmail());
+		System.out.println("team: " + theCoach2.getTeam());
 		
 		//close the context
 		context.close();
